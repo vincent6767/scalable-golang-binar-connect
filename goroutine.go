3853c59dfc37f90a6843828b00d61511957e146a
Vincent <vincentbiasa6767@gmail.com>
2017-12-10 13:59:02 +0700

    add channel implementation

--- goroutine.go
+++ goroutine.go
@@ -16,6 +16,8 @@ type PayloadCollection struct {
 	Payloads []*Payload
 }
 
+var payloadChan = make(chan *Payload, 200)
+
 // UploadToSomewhere simulates the data upload
 func (p *Payload) UploadToSomewhere() bool {
 	var sum int
@@ -27,7 +29,7 @@ func (p *Payload) UploadToSomewhere() bool {
 
 // EventHandler handles the event endpoint.
 func EventHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
+	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
@@ -44,4 +46,33 @@ func EventHandler(w http.ResponseWriter, r *http.Request) {
 		go payload.UploadToSomewhere()   // <----- What do you think will happen?
 	}
 	w.WriteHeader(http.StatusOK)
+}
+
+func EventHandlerChannel(w http.ResponseWriter, r *http.Request){
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	var content = &PayloadCollection{}
+	err := json.NewDecoder(io.LimitReader(r.Body, MaxLength)).Decode(&content)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	for _, payload := range content.Payloads {
+		payloadChan <- payload  // <----- The execution will be blocked when the channel hit 100. You're welcome
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func handlePayload(pc chan *Payload) {
+	for {
+		select {
+		case payload := <- pc:
+			payload.UploadToSomewhere()
+		}
+	}
 }
